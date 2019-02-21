24d64a7154ef3a097574732b41b3b4c0dd99b1cf
Hizkia Setiadi <hizkia.setiadi@gmail.com>
2019-02-21 16:26:55 +0700

    - Add Name detection (assumption: Name always below NIK)

--- app/src/main/java/com/arikachmad/ocrektp/textrecognition/TextRecognitionProcessor.kt
+++ app/src/main/java/com/arikachmad/ocrektp/textrecognition/TextRecognitionProcessor.kt
@@ -18,9 +18,12 @@ import java.util.regex.Pattern
 class TextRecognitionProcessor : VisionProcessorBase<FirebaseVisionText>() {
 
     private val detector: FirebaseVisionTextRecognizer = FirebaseVision.getInstance().onDeviceTextRecognizer
-    private var listener : ((String) -> Unit)? = null
+    private var listener: ((String) -> Unit)? = null
 
-
+    var nik: String = ""
+    var nikIndex: Int = -1
+    var nama: String = ""
+    var namaIndex: Int = -1
     override fun stop() {
         try {
             detector.close()
@@ -39,30 +42,41 @@ class TextRecognitionProcessor : VisionProcessorBase<FirebaseVisionText>() {
         frameMetadata: FrameMetadata,
         graphicOverlay: GraphicOverlay
     ) {
+        nik = ""
+        nama = ""
+        nikIndex = -1
+        namaIndex - 1
+
         graphicOverlay.clear()
         originalCameraImage?.let { image ->
             val imageGraphic = CameraImageGraphic(graphicOverlay, image)
             graphicOverlay.add(imageGraphic)
         }
-        val blocks = results.textBlocks
-        for (i in blocks.indices) {
-            val lines = blocks[i].lines
-            for (j in lines.indices) {
-                val elements = lines[j].elements
-                for (k in elements.indices) {
-                    val textGraphic = TextGraphic(graphicOverlay, elements[k])
-                    graphicOverlay.add(textGraphic)
-                    val regexKtpPattern = "[0-9]{16}"
-                    val pattern = Pattern.compile(regexKtpPattern)
-                    val matcher = pattern.matcher(elements[k].text)
-                    if (matcher.find()) {
-                        listener?.invoke(matcher.group())
-                    }
-
-                }
-            }
+        val lines = results.text.lines()
+        for ((index, value) in lines.withIndex()) {
+            recognizeNIK(index, value)
+        }
+        if (namaIndex > -1 && namaIndex < lines.size) {
+            var numeric = true
+            numeric = lines[namaIndex].matches("-?\\d+(\\.\\d+)?".toRegex())
+            if (!numeric && lines[namaIndex].indexOf(",") == -1 && lines[namaIndex].indexOf("-") == -1)
+                nama = lines[namaIndex]
+        }
+        if (!nik.isEmpty() && !nama.isEmpty()) {
+            listener?.invoke("NIK : $nik\nNama : $nama")
         }
         graphicOverlay.postInvalidate()
+    }
+
+    fun recognizeNIK(index: Int, text: String) {
+        val regexKtpPattern = "[0-9]{16}"
+        val pattern = Pattern.compile(regexKtpPattern)
+        val matcher = pattern.matcher(text)
+        if (matcher.find()) {
+            nik = matcher.group()
+            nikIndex = index
+            namaIndex = nikIndex + 1
+        }
     }
 
     override fun onFailure(e: Exception) {
